5c8dd4f1a4c0ae7a631a6c1078e332289cdd9c32
Seunghwan Yoon <y305205@gmail.com>
2021-08-23 13:18:45 +0900

    Fix result page

--- src/atoms/Result.js
+++ src/atoms/Result.js
@@ -1,8 +1,16 @@
 import React, {useEffect, useState} from "react";
 import "./Result.scss"
-
+const Col = ({A,B}) => {
+    return <div><div>{B}</div>{A?.map((x,i)=><div>{i}반: {x}등</div>)}</div>
+}
 const Result = ({data,dataRef,admin,classID,t}) => {
-    const downstream = data?.RESULT;
-    return <div>Result<br/>{JSON.stringify(downstream)}</div>
+    const A = [data?.ranking?.R,data?.ranking?.G,data?.ranking?.H,data?.RESULT?.allRanking];
+    const B = ["R","G","H","A"];
+    return <div>
+        <h1>Result(To Be Designed)</h1><br/>
+        <div style={{"display":"flex",justifyContent: "space-around"}}>
+            {A.map((x,i)=><Col A={x} B={B[i]}/>)}
+        </div>
+    </div>
 }
 export default Result;
